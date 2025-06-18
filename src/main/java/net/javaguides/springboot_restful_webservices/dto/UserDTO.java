a832c12e67c9fa0a52ad831b5de6dbd359812ea7
chrisann.lee <chrisannlee97@gmail.com>
2025-06-18 10:59:55 +0800

    spring boot documentation (model) - swagger

--- src/main/java/net/javaguides/springboot_restful_webservices/dto/UserDTO.java
+++ src/main/java/net/javaguides/springboot_restful_webservices/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot_restful_webservices.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "User DTO Model Information"
+)
 @Setter
 @Getter
 @AllArgsConstructor
@@ -14,9 +18,18 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     @NotEmpty(message = "User first name should not be null or empty.")
+    @Schema(
+            description = "User First Name"
+    )
     private String firstName;
+    @Schema(
+            description = "User Last Name"
+    )
     @NotEmpty(message = "User last name should not be null or empty.")
     private String lastName;
+    @Schema(
+            description = "User Email"
+    )
     @NotEmpty(message = "User email should not be null or empty.")
     @Email(message = "Email address should be valid.")
     private String email;
